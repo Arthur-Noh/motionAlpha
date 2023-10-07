20905e8df8a592b396628d694586e08ff0027c40
Arthur-Noh <dkagod97@gmail.com>
2023-10-07 19:38:56 +0900

    feat: store, storage

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,10 @@ module.exports = {
     root: true,
     extends: '@react-native',
     rules: {
+        // 인라인 스타일 허용
         'react-native/no-inline-styles': 0,
+        // 미사용 변수 에러 끄기
+        'no-unused-var': 'off',
+        '@typescript-eslint/no-unused-vars': 'off',
     },
 };
